b2dcbb51ac6c8190d27b7726b210c5397ce942d8
clardizabal <chris.lardizabal@gmail.com>
2016-08-24 14:59:16 -0700

    show labels on markers

--- client/controllers.js
+++ client/controllers.js
@@ -5,10 +5,11 @@ angular.module('app.controllers', [])
 .controller('SearchCtrl', function($scope, $mdDialog, Location, GoogleSearch) {
   $scope.query;
   $scope.places = [];
+  $scope.markers = [];
   $scope.latitude;
   $scope.longitude;
 
-  $scope.markers = [];
+  /* map options */
   $scope.options = {
     styles: [
       {
@@ -25,22 +26,65 @@ angular.module('app.controllers', [])
     .then(function(data) {
       $scope.places = data.results;
 
-      /* Find lat and lng of each place and create a marker */
-      $scope.places.forEach(function(place, index) {
+      /* Remove extra query results from places array */
+      $scope.places.splice(resultsLimit, GOOGLE_QUERY_RESULTS - resultsLimit);
+      /* zIndex property added to scope for mouseover event */
+      $scope.zIndex = resultsLimit;
 
+      /* Find lat and lng of each place and create a marker to be added to map */
+      $scope.places.forEach(function(place, index) {
+        /* Assign place with a label that associates with map */
+        $scope.places[index].label = letters.charAt(index);
         var marker = {
           latitude: place.geometry.location.lat,
           longitude: place.geometry.location.lng,
-          title: index + 1,
-          id: index + 1
+          id: index + 1,
+          options: {
+            zIndex: $scope.zIndex--,
+            label: {
+              text: $scope.places[index].label,
+              color: 'white',
+              fontSize: '18px',
+              fontWeight: 'bold'
+            }
+          }
         };
         $scope.markers.push(marker);
       });
     });
   };
 
+  /* EVENT HANDLER FOR CLICK ON SEARCH RESULTS */  
+  $scope.showAdvanced = showAdvanced;
+
+  /* EVENT HANDLER FOR MARKERS */
+  $scope.eventsObject = {
+        mouseover: markerMouseOver,
+  };
+
+  /* On load, get location of the client */
+  Location.search().then(function(position) {
+    $scope.latitude = position.coords.latitude;
+    $scope.longitude = position.coords.longitude;
+    
+    $scope.map = {
+      center: {
+        latitude: $scope.latitude,
+        longitude: $scope.longitude
+      },
+      zoom: 13,
+    };
+  });
+
+
+  /* Bring marker to fron by setting zIndex to highest value */
+  function markerMouseOver(marker, e, m) {
+      $scope.zIndex = ++resultsLimit;
+      m.options.zIndex = $scope.zIndex;
+  }
+
   /* On click function to show more details */
-  $scope.showAdvanced = function(place, ev) {
+  function showAdvanced(place, ev) {
 
     GoogleSearch.details(place.place_id)
 
@@ -77,20 +121,5 @@ angular.module('app.controllers', [])
     $scope.status = data.opening_hours ? (data.opening_hours.open_now ? 'OPEN' : 'CLOSED') : '';
     console.log($scope.status);
   }
-
-  /* On load, get location of the client */
-  Location.search().then(function(position) {
-    $scope.latitude = position.coords.latitude;
-    $scope.longitude = position.coords.longitude;
-
-    $scope.map = {
-      center: {
-        latitude: $scope.latitude,
-        longitude: $scope.longitude
-      },
-      zoom: 13,
-    };
-  });
-
 });
 
